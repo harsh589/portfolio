a02d99d0659483bfeba22ccc3f2d1343b31b742f
harsh589 <85626754+harsh589@users.noreply.github.com>
2023-07-07 00:50:41 +0530

    added elements

---
+++ portfolio.js
@@ -0,0 +1,65 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+/*scroll section active link*/
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offset - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+  /* scroll section active link */
+
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  //////////////////////remove toggle icon navbar//////////////////////
+
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
+
+
+//////////////////////////scroll navbar ///////////////////////////
+ScrollReveal({ 
+  // reset: true,
+    distance: '80px',
+     duration:2000,
+     delay:200
+});
+
+ScrollReveal().reveal('.home-content ,.heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img ,.services-container,.portfolio-box,.contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1 ,.about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p,.about-content', { origin: 'right' });
+
+
+//////////////typed js///////////////////
+
+const typed  = new Typed('.multiple-text',{
+    strings:['Frontend Developer','Backend Developer','Web Developer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+});
